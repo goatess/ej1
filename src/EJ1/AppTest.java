eaa68bda3f5951c2c5683e554b96c52927f694e4
goatess <107187023+goatess@users.noreply.github.com>
2022-06-27 11:49:04 +0200

    first commit

---
+++ src/EJ1/AppTest.java
@@ -0,0 +1,18 @@
+package EJ1;
+import static org.junit.Assert.assertEquals; 
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test(){
+
+        //arrange
+
+        //act
+        
+        //assert
+        assertEquals(true, true);
+    } 
+    
+}
